8aaf5839b44a794db0e077059259ce5210aaa4e0
Egor Yakubov <e.yakubov@aks.group>
2018-03-22 13:29:58 +0400

    Убрал вывод ошибки о том, что каталог AKS не найден. Упростил скрипт.

--- install_card.cmd
+++ install_card.cmd
@@ -1,10 +1,8 @@
 @echo off
 set aks_path=%appdata%\AKS
+mkdir %aks_path% > NUL 2>&1
 cd %aks_path%
-if %errorlevel% NEQ 0 (
-    mkdir %aks_path%
-    cd %aks_path%
-)
+
 powershell.exe -Command "(New-Object Net.WebClient).DownloadFile('http://file.aks.support/cardinstall.ps1', 'cardinstall.ps1')"
 powershell.exe -executionpolicy RemoteSigned -file cardinstall.ps1
 del cardinstall.ps1
