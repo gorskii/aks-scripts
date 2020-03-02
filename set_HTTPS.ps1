9367f45982c829ecdca07bc2d35fc4d6be09b3d2
Egor Yakubov <e.yakubov@aks.group>
2020-03-02 16:33:07 +0400

    Added set_HTTPS.ps1
    This script replaces HTTP connection strings with HTTPS ones in iikoFront's 'config.xml' and iikoDelivery plugin's 'deliveryPluginConfig.xml'.
    It also searches for all available BackOffice config files in '%AppData%\iiko' folder and sets HTTPS protocol for every HTTP entry.
    
    Скрипт заменяет строки подключения по протоколу HTTP на HTTPS в файле
    конфигурации iikoFront 'config.xml' и в файле конфигурации плагина
    iikoDelivery 'deliveryPluginConfig.xml'.
    Также он ищет все доступные файлы конфигурации BackOffice в каталоге '%AppData%\iiko' и заменяет протокол подключения на HTTPS для каждой HTTP-записи.

---
+++ set_HTTPS.ps1
@@ -0,0 +1,78 @@
+﻿<#
+.SYNOPSIS
+
+    Set connection protocol to HTTPS for iikoFront, iikoOffice and iikoChain
+
+.DESCRIPTION
+
+    This script replaces HTTP connection strings with HTTPS ones in iikoFront's 'config.xml' and iikoDelivery plugin's 'deliveryPluginConfig.xml'.
+    It also searches for all available BackOffice config files in '%AppData%\iiko' folder and sets HTTPS protocol for every HTTP entry.
+
+    NOTE: Only iikoCloud entries (iiko.it:8080 or iiko.it:9080) are affected.
+
+#>
+
+$PATH_RMS = "$env:APPDATA\iiko\Rms"
+$PATH_CHAIN = "$env:APPDATA\iiko\Chain"
+$PATH_CASHSERVER = "$env:APPDATA\iiko\CashServer"
+
+Function SetHTTPS([String]$path){
+    if (Test-Path $path)
+    {
+        $configFile = Get-Content -ErrorAction Stop -Path $path
+        if (($configFile | Select-String "iiko.it:8080/resto") -or (($configFile | Select-String "iiko.it") -and (($configFile | Select-String "<Port>8080</Port>") -or ($configFile | Select-String "<Port>9080</Port>"))))
+        {
+            $configFile = $configFile -replace "http://", "https://"
+            $configFile = $configFile -replace "<Protocol>http</Protocol>", "<Protocol>https</Protocol>"
+            $configFile = $configFile -replace "<Port>8080</Port>", "<Port>443</Port>"
+            $configFile = $configFile -replace "<Port>9080</Port>", "<Port>443</Port>"
+            $configFile = $configFile -replace "iiko.it:8080/resto", "iiko.it/resto"
+            $configFile -replace "iiko.it:9080/resto", "iiko.it/resto" | Out-File $path -Encoding default -ErrorAction Stop
+            Write-Host "Succsessfully set HTTPS entries in '$path'."
+        }
+        else
+        {
+            Write-Host "No HTTP iikoCloud entries found in '$path'."
+        }
+    }
+    else
+    {
+        Write-Host "File '$path' does not exist."
+    }
+    return
+} #end SetHTTPS
+
+if (Test-Path $PATH_RMS)
+{
+    ForEach ($FolderName in Get-ChildItem $PATH_RMS) {
+        $path = "$PATH_RMS\$FolderName\config\backclient.config.xml"
+        SetHTTPS($path)
+    }
+}
+else
+{
+    Write-Host "Path '$PATH_RMS' does not exist."
+}
+
+if (Test-Path $PATH_CHAIN)
+{
+    ForEach ($FolderName in Get-ChildItem $PATH_CHAIN) {
+        $path = "$PATH_CHAIN\$FolderName\config\backclient.config.xml"
+        SetHTTPS($path)
+    }
+}
+else
+{
+    Write-Host "Path '$PATH_CHAIN' does not exist."
+}
+
+if (Test-Path $PATH_CASHSERVER)
+{
+    SetHTTPS("$PATH_CASHSERVER\config.xml")
+    SetHTTPS("$PATH_CASHSERVER\PluginConfigs\Resto.Front.Api.Delivery\deliveryPluginConfig.xml")
+}
+else
+{
+    Write-Host "Path '$PATH_CASHSERVER' does not exist."
+}
+Start-Sleep 10
