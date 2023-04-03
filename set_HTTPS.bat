2ddeea4d096328babbede9f98a845b86011f5960
Egor Yakubov <52618928+gorskii@users.noreply.github.com>
2023-04-03 16:47:10 +0400

    Fix broken download URL in set_HTTPS.bat

--- set_HTTPS.bat
+++ set_HTTPS.bat
@@ -3,6 +3,6 @@ set aks_path=%appdata%\AKS
 mkdir %aks_path% > NUL 2>&1
 cd %aks_path%
 
-powershell.exe -Command "(New-Object Net.WebClient).DownloadFile('http://files.aks.group/set_HTTPS.ps1', 'set_HTTPS.ps1')"
+powershell.exe -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/gorskii/aks-scripts/master/set_HTTPS.ps1', 'set_HTTPS.ps1')"
 powershell.exe -executionpolicy RemoteSigned -file set_HTTPS.ps1
 del set_HTTPS.ps1
